d44937ac0b6820fc37cab10e920cd2e99dfdfe6c
loisblin <blinlois@gmail.com>
2024-01-13 17:38:29 +0100

    [FEature]normalise

--- normalisation.py
+++ normalisation.py
@@ -1,6 +1,11 @@
 from sklearn.preprocessing import StandardScaler
+def variableCible(df):
+    return df["trip_duration"]
+def variablePredictive(df):
+    variables= ["day", "hour", "PULocationID", "DOLocationID"]
+    return df[variables]
 def normalise(df):
     scale = StandardScaler()
-
-    df_scaled = scale.fit_transform(df[["day", "hour", "PULocationID", "DOLocationID"]].as_matrix())
+    vp=variablePredictive(df)
+    df_scaled = scale.fit_transform(vp[["day", "hour", "PULocationID", "DOLocationID"]].as_matrix())
     return df_scaled
